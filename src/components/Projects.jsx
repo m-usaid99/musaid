007a3ea44df2ca3ddf47bf2b6ac3f8d58e6ca753
Muhammad Usaid Rehman <rehman.usaid@gmail.com>
2024-08-12 17:39:29 +0500

    initiated project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Box from './Box';
+import styles from '../styles/Projects.module.css'; // Assuming you'll use CSS modules
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects Page</h1>
-    </div>
+    <Box>
+      <div className={styles.content}>
+        <div className={styles.leftColumn}>
+          <h1 className={styles.title}>PROJECTS</h1>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className={styles.rightColumn}>
+          {/* Right column content for the project list will go here */}
+        </div>
+      </div>
+    </Box>
   );
 }
 
