e114315147ef3ff1c48f9ee019d13f324a6e874c
Muhammad Usaid Rehman <rehman.usaid@gmail.com>
2024-08-14 03:48:19 +0500

    added splash component

---
+++ src/components/Splash.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from '../styles/Splash.styles.css';
+
+const Splash = () => {
+  return (
+    <div>
+      <p className={styles.name}>MUHAMMAD USAID</p>
+      <p className={styles.label}>portfolio</p>
+    </div>
+
+  )
+};
+
+export default Splash;
