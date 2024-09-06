e1a7158bee38e837a80ce772656823c3a941d805
Muhammad Usaid Rehman <rehman.usaid@gmail.com>
2024-09-06 22:27:55 +0500

    created sketch for projects

--- src/components/projects/ExpenseMate.jsx
+++ src/components/projects/ExpenseMate.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Box from "../Box";
+import Sketch from "../Sketch";
+import { arcDraw, arcSetup } from "../sketches/arcSketch";
 
 
 function ExpenseMate() {
   return (
-    <Box>
+    <Box sketch={<Sketch setup={arcSetup} draw={arcDraw} />}>
       <h1>Expense Mate</h1>
     </Box>
   );
---
+++ src/components/sketches/arcSketch.jsx
@@ -0,0 +1,53 @@
+class Arc {
+  constructor(p, r, f) {
+    this.p = p;
+    this.r = r;
+    this.angle = 0;
+    this.f = f;
+    this.length = 180;
+    this.angleV = 0.01;  // Further reduced angular velocity
+  }
+
+  display() {
+    this.p.noFill();
+
+    // Big arc
+    this.p.strokeWeight(1);
+    this.p.stroke(220, 30);
+    this.p.arc(0, 0, this.r * 2, this.r * 2, (this.angle * this.f) - this.length, this.angle * this.f);
+    this.x = this.r * this.p.cos(this.angle * this.f);
+    this.y = this.r * this.p.sin(this.angle * this.f);
+    this.p.fill(220, 50);
+    this.p.ellipse(this.x, this.y, 1, 1);
+  }
+
+  update() {
+    this.angle += this.angleV;  // Even slower increment
+  }
+}
+
+export const arcSetup = (p) => {
+  p.arcs = [];
+  let num = 10;
+  let r_mult = 1.25;
+  let r0 = 150;
+  let f_mult = 0.2;
+
+  // Create a number of arcs
+  for (let i = 0; i < num; i++) {
+    let r = r0 + ((i + 1) * num * r_mult);
+    let f = (i + 1) * f_mult;
+    p.arcs.push(new Arc(p, r, f));
+  }
+};
+
+export const arcDraw = (p) => {
+  p.background(10);
+  p.translate(p.width / 2, p.height / 2);
+
+  // Update and display each arc
+  p.arcs.forEach(arc => {
+    arc.update();
+    arc.display();
+  });
+};
